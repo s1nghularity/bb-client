e1c86a738dd0bcb9bb5732badeca9a426406dae7
Vikram Singh <v.p.singh@gmail.com>
2023-02-13 17:32:48 -0800

    Update useUserData.js
    updaeted useEffect to regain functionality...wip

--- src/useUserData.js
+++ src/useUserData.js
@@ -3,25 +3,26 @@ import axios from 'axios';
 
 const useUserData = () => {
   const [userData, setUserData] = useState({});
-
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchUserData = async () => {
       const result = await axios.post('https://bb-server-8r19.onrender.com/userData', {
         token: window.localStorage.getItem('token'),
       });
       setUserData(result.data.data);
+      setLoading(false);
       
     };
 
     fetchUserData();
-  }, []);
+  }, [setUserData]);
 
   const logOut = () => {
     window.localStorage.removeItem('token');
     window.location.reload();
   };
 
-  return { userData, logOut };
+  return { userData, logOut, loading };
 };
 
 export default useUserData;
